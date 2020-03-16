3fa49f01403d6a23e81f3d2b32ab23c7bbd7fa5c
Michał Gruca <pucek9@gmail.com>
2020-03-16 01:01:37 +0100

    up

--- src/server/services/Steering/Steering.ts
+++ src/server/services/Steering/Steering.ts
@@ -12,18 +12,7 @@ export default abstract class Steering {
 
   performWeaponChange(game, player) {
     const shift = player.keys.has('Shift');
-    const [
-      weapon1,
-      weapon2,
-      weapon3,
-      weapon4,
-      weapon5,
-      weapon6,
-      weapon7,
-      weapon8,
-      weapon9,
-      weapon10,
-    ] = [
+    const weapons = [
       player.keys.has('1'),
       player.keys.has('2'),
       player.keys.has('3'),
@@ -35,35 +24,35 @@ export default abstract class Steering {
       player.keys.has('9'),
       player.keys.has('0'),
     ];
-    const weaponPressed = [weapon1, weapon2, weapon3, weapon4, weapon5].some(has => has);
-    if (weapon1 && !shift) {
+    const weaponPressed = weapons.some(has => has);
+    if (weapons[0] && !shift) {
       player.selectWeapon(0);
     }
-    if (weapon2 && !shift) {
+    if (weapons[1] && !shift) {
       player.selectWeapon(1);
     }
-    if (weapon3 && !shift) {
+    if (weapons[2] && !shift) {
       player.selectWeapon(2);
     }
-    if (weapon4 && !shift) {
+    if (weapons[3] && !shift) {
       player.selectWeapon(3);
     }
-    if (weapon5 && !shift) {
+    if (weapons[4] && !shift) {
       player.selectWeapon(4);
     }
-    if (weapon6 && !shift) {
+    if (weapons[5] && !shift) {
       player.selectWeapon(5);
     }
-    if (weapon7 && !shift) {
+    if (weapons[6] && !shift) {
       player.selectWeapon(6);
     }
-    if (weapon8 && !shift) {
+    if (weapons[7] && !shift) {
       player.selectWeapon(7);
     }
-    if (weapon9 && !shift) {
+    if (weapons[8] && !shift) {
       player.selectWeapon(8);
     }
-    if (weapon10 && !shift) {
+    if (weapons[9] && !shift) {
       player.selectWeapon(9);
     }
     if (weaponPressed && !shift) {
@@ -73,7 +62,7 @@ export default abstract class Steering {
 
   performPowerChange(game, player) {
     const shift = player.keys.has('Shift');
-    const [power1, power2, power3, power4, power5, power6, power7, power8, power9, power10] = [
+    const powers = [
       player.keys.has('!'),
       player.keys.has('@'),
       player.keys.has('#'),
@@ -85,38 +74,39 @@ export default abstract class Steering {
       player.keys.has('('),
       player.keys.has(')'),
     ];
-    const powerPressed = [power1, power2, power3, power4, power5].some(has => has);
+    const powerPressed = powers.some(has => has);
     if (shift && powerPressed) {
+      console.log('released');
       player.releasePower(game);
     }
-    if (shift && power1) {
+    if (shift && powers[0]) {
       player.selectPower(0);
     }
-    if (shift && power2) {
+    if (shift && powers[1]) {
       player.selectPower(1);
     }
-    if (shift && power3) {
+    if (shift && powers[2]) {
       player.selectPower(2);
     }
-    if (shift && power4) {
+    if (shift && powers[3]) {
       player.selectPower(3);
     }
-    if (shift && power5) {
+    if (shift && powers[4]) {
       player.selectPower(4);
     }
-    if (shift && power6) {
+    if (shift && powers[5]) {
       player.selectPower(5);
     }
-    if (shift && power7) {
+    if (shift && powers[6]) {
       player.selectPower(6);
     }
-    if (shift && power8) {
+    if (shift && powers[7]) {
       player.selectPower(7);
     }
-    if (shift && power9) {
+    if (shift && powers[8]) {
       player.selectPower(8);
     }
-    if (shift && power10) {
+    if (shift && powers[9]) {
       player.selectPower(9);
     }
     if (shift && !powerPressed) {
